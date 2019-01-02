e54325c7880e2d8de2b8b76ef31be9f2364e9d3a
daniel.pontes <daniel.pontes@capgemini.com>
2019-01-02 15:24:59 -0300

    Criação do context

---
+++ src/FMP.Infrastructure/Data/FisioContext.cs
@@ -0,0 +1,22 @@
+﻿using FMP.ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FMP.Infrastructure.Data
+{
+    public class FisioContext : DbContext
+    {
+        public FisioContext(DbContextOptions<FisioContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Cliente");
+        }
+
+        
+    }
+}
