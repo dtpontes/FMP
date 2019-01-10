f9d3eb6940b52df7e145c0c5cf9a87e7855a1e38
daniel.pontes <daniel.pontes@capgemini.com>
2019-01-10 16:49:03 -0300

    Ajustes

---
+++ src/FMP.ApplicationCore/Validators/PacienteValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using FMP.ApplicationCore.Entities;
+using System;
+
+namespace FMP.ApplicationCore.Validators
+{
+    public class PacienteValidator : AbstractValidator<Paciente>
+    {
+        public PacienteValidator()
+        {
+            RuleFor(c => c)
+                .NotNull()
+                .OnAnyFailure(x =>
+                {
+                    throw new ArgumentNullException("Can't found the object.");
+                });
+
+            RuleFor(c => c.CPF)
+                .NotEmpty().WithMessage("Is necessary to inform the CPF.")
+                .NotNull().WithMessage("Is necessary to inform the CPF.");
+
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Is necessary to inform the name.")
+                .NotNull().WithMessage("Is necessary to inform the name.");
+        }
+    }
+}
